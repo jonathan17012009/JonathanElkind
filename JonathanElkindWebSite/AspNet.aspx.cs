e74eccc1b708600a59c8a5221956d43e816e3fc1
User Omer <User@ROOM302-17>
2025-12-31 12:15:16 +0200

    10x10

---
+++ JonathanElkindWebSite/AspNet.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class AspNet : System.Web.UI.Page
+{
+    public string kefel;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        kefel = "<table border ='1'>";
+        for (int i = 1; i<=10; i++)
+        {
+            kefel += "<tr>";
+            for (int j = 1; j<=10; j++)
+            {
+                kefel += "<td>" + (i * j) + "</td>";
+            }
+            kefel += "</tr>";
+        }
+        kefel += "</table>";
+    }
+}
+
